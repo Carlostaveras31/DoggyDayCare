751a55052898872f43a8b289365eb4b45cf8b433
Carlos Taveras <cthirst@carloss-mbp.myfiosgateway.com>
2021-06-29 19:04:29 -0400

    Created Animal, Dog, Cat

---
+++ src/com/company/Cat.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Cat extends Animal{
+
+    public String pattern;
+    public boolean wasStray;
+    public int killCount;
+
+
+    public Cat(
+            int legs,
+            String size,
+            boolean isFixed,
+            String name,
+            double weight,
+            String pattern,
+            boolean wasStray,
+            int killCount
+    ) {
+        super(legs, size, isFixed, name, weight);
+        this.pattern = pattern;
+        this.wasStray = wasStray;
+        this.killCount = killCount;
+    }
+
+    @Override
+    public void speak(){
+        if (killCount > 5){
+            System.out.println("Hiss");
+        }else {
+            System.out.println("Meow");
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello");
+	Animal frog = new Animal(4,"small",false,"Raul",2.0);
+	Cat cat = new Cat(
+	        4,
+            "Extra Large",
+            true,
+            "Jay",
+            12.0,
+            "Calico",
+            true,
+            10
+    );
+	Dog dog = new Dog(
+	        2,
+            "Medium",
+            true,
+            "Benji",
+            27.7,
+            "Bulldog",
+            "short",
+            "834"
+    );
+
+	frog.speak();
+	cat.speak();
+	dog.speak();
     }
 }
