cf1fe7ce1b3f7c9eb199618de815fcca3ad5155b
Carlos Taveras <cthirst@carloss-mbp.myfiosgateway.com>
2021-06-28 20:56:17 -0400

    adding fields and constructor

---
+++ src/com/company/Animal.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Animal {
+
+    public int legs;
+    public String size;
+    public boolean isFixed;
+    public String name;
+    public double weight;
+
+    public Animal(int legs, String size, boolean isFixed, String name){
+        this.legs = legs;
+        this.size = size;
+        this.name = name;
+        this.isFixed = isFixed;
+    }
+
+}
